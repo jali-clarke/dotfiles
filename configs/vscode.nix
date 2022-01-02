b9ac1874fe60d3d343a810908a44d9b826a15845
Jinnah Ali-Clarke <jinnah@noredink.com>
2022-01-02 14:44:47 -0500

    nixpkgs-fmt .

--- configs/vscode.nix
+++ configs/vscode.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    extensions = let exts = pkgs.vscode-extensions;
-    in [
-      exts._4ops.terraform
-      exts.bbenoist.nix
-      exts.brettm12345.nixfmt-vscode
-      exts.dhall.dhall-lang
-      exts.eamodio.gitlens
-      exts.justusadam.language-haskell
-    ];
+    extensions =
+      let exts = pkgs.vscode-extensions;
+      in
+      [
+        exts._4ops.terraform
+        exts.bbenoist.nix
+        exts.brettm12345.nixfmt-vscode
+        exts.dhall.dhall-lang
+        exts.eamodio.gitlens
+        exts.justusadam.language-haskell
+      ];
 
     userSettings = {
       "files.autoSave" = "afterDelay";
