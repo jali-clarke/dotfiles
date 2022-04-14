bc50369a8eb4b9a2e516d9d98dcf9cdbd5ea2abd
Jinnah Ali-Clarke <jinnah@noredink.com>
2022-04-14 11:40:06 -0400

    add elm vscode ext

--- configs/vscode/extensions/default.nix
+++ configs/vscode/extensions/default.nix
@@ -5,6 +5,7 @@
   vscode-extensions.brettm12345.nixfmt-vscode
   vscode-extensions.dhall.dhall-lang
   vscode-extensions.eamodio.gitlens
+  vscode-extensions.elmtooling.elm-ls-vscode
   vscode-extensions.justusadam.language-haskell
 
   (pkgs.callPackage ./cuelang.nix { })
